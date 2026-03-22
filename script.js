26293c33fa5f8bc2850b791dacd0176b47a7968d
Ryan D. <208831299+techbug-ux@users.noreply.github.com>
2026-03-22 01:35:54 -0400

    Add the function getComputerChoice
    The functions works by select three numbers 0,1,2 which are assigned to make choice of rock, paper, scissors. Then the function is printed to main.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello, World")
+
+function getComputerChoice(num) {
+    if (Math.floor(Math.random() * num) == 0){
+        return "rock";
+    } else if (Math.floor(Math.random() * num) == 1){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice(3))
