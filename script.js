5f21e787b16874346234a81c924a02781753b336
Ryan D. <208831299+techbug-ux@users.noreply.github.com>
2026-03-26 11:38:09 -0400

    Created the algorithm for getComputerChoice
    It uses math.random by using 3 to select which choice then is hardcoded 0 = rock, 1 = paper, etc.

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 // HELLO WORLD TEST
+//Step 1: Print Hello World in console
 console.log("Hello World");
+
+//Step 2: create function getComputerChoice
+//it must return "rock", "paper", "scissors"
+//math.random is to select three random choices
+
+function getComputerChoice() {
+    //make condtional to return each output
+    if (Math.floor(Math.random() * 3) == 0 ) {
+        return "rock";
+    }
+    else if (Math.floor(Math.random() * 3) == 1 ) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+//testing with console log for function getComputerChoice
+console.log(getComputerChoice());
